bdf4cd28188bac0d27edf1d99b1de4ee33c793b8
Zitai Huang <mywpiedu@gmail.com>
2020-01-12 12:25:56 +0000

    Automatically backed up by Learn

--- lib/custom_errors.rb
+++ lib/custom_errors.rb
@@ -7,8 +7,19 @@ class Person
 
   def get_married(person)
     self.partner = person
-    person.partner = self
+    if person.class != Person 
+      raise PartnerError
+    else 
+      person.partner = self
+    end 
   end
+
+  class PartnerError < StandardError 
+    
+    
+  end 
+
+
 
 end
 
